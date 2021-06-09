314a6964010a92cd814456f9205aa83f524acb6f
fredsonandrade <fredsondias@gmail.com>
2021-06-09 00:41:53 -0300

    Conferir sencura e meia entrada no cinema

---
+++ ApenasParaMaiores.js
@@ -0,0 +1,11 @@
+function solucao(idade) {
+    //seu codigo aqui
+      if(idade < 0){
+         console.log("IDADE INVALIDA");
+      } else if (idade < 18){
+          console.log("ACESSO NEGADO");
+      } else {
+          console.log("PODE ENTRAR");
+      }
+    
+}
